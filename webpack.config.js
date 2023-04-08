c323067694d5ae3c664b089b5b016ce15abfb0f5
Dylan Hawkins <dylan.andrew.hawkins@gmail.com>
2023-04-07 20:53:23 -0400

    Set up webpack & node modules

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './client/index.html'
+    })
+  ],
+  mode: process.env.NODE_ENV,
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader', 'css-loader', 'sass-loader'
+        ],
+      },
+    //   {
+    //     test: /\.png|svg|jpg|gif$/,
+    //     use: [
+    //         'file-loader'
+    //     ],
+    //  }, 
+    // {
+    //     test: /\.png|svg|jpg|gif$//,
+    //     type: 'asset/resource'
+    //   }
+    ]
+  },
+  devServer: {
+    static: [
+      path.join(__dirname, 'client')
+    ]
+
+     // proxy setting to be included
+    //  proxy: {
+    //     '/api/**': {
+    //         target: 'http://localhost:3000/',
+    //         secure: false,
+    //       },
+    //   },
+    // // fallback to root for other urls
+    // historyApiFallback: true,
+  },
+  resolve: {
+    // Enable importing JS / JSX files without specifying their extension
+    extensions: ['.js', '.jsx'],
+  },
+}
+
+
+
