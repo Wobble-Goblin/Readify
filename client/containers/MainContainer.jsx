55aafc066e1044f563f84866230b286328d0238e
Dylan Hawkins <dylan.andrew.hawkins@gmail.com>
2023-04-08 12:50:33 -0400

    Set up basic front end file structure
    Co-authored-by: Matteo diterlizzi.matteo@gmail.com
    Co-authored-by: Das dahae0309@hotmail.com
    Co-authored-by: Jasmine jasminee@gmail.com

---
+++ client/containers/MainContainer.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+// import from child stuff
+
+function MainContainer() {
+    return (
+        <div>Main Container content! </div>
+    )
+}
+
+
+export default MainContainer;
