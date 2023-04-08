720c4c5451c0c2dba18483d64d9a56f9bc4a8e3c
Jasmine Noor <jasminee@gmail.com>
2023-04-08 16:30:31 -0400

    Combining front end and back end
    Co-authored-by Das dahae0309@hotmail.com
    Co-authored-by Dylan dylan.andrew.hawkins@gmail.com
    Co-autored-by Jasmine jasminee@gmail.com
    Co-autored-by Matteo diterlizzi.matteo@gmail.com

--- server/router.js
+++ server/router.js
@@ -5,7 +5,7 @@ const controller = require('./controller');
 // GETS SPOTIFY TOKEN
 // get-token creating an access token
 
-  router.post('/get-token',
+router.post('/get-token',
   controller.getToken, (err, res, body) => {
     if (!err && res.statusCode === 200) {
       const token = body.access_token;
@@ -14,15 +14,15 @@ const controller = require('./controller');
 
 // GET BOOK
 router.get('/get-title',
-controller.getTitle,
-(req,res) => res.status(200).json(res.locals.books))
+  controller.getTitle,
+  (req, res) => res.status(200).json(res.locals.books))
 
 // TEST SAMPLE ONLY ONE ADDRESS FOR EACH REQUEST
 //serve index html
 router.get('/', (req, res) => {
-    return res
-      .status(200)
-      .send('sendingggg') //working
-  });
+  return res
+    .status(200)
+    .send('sendingggg') //working
+});
 
 module.exports = router;
